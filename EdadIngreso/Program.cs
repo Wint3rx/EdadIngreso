6916e1fe15f5bafd1e86800f48422d9d9021fb66
Winter <Winter@DESKTOP-U9PL0DD>
2024-02-20 22:22:31 -0600

    Agregar archivos de proyecto.

---
+++ EdadIngreso/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese su edad:");
+        double n1 = Convert.ToDouble(Console.ReadLine());
+
+        if (n1 < 18)
+        {
+            Console.WriteLine("Lamentablemente usted no es apto para entrar a este club :c");
+        }
+        else
+        {
+            Console.WriteLine("Usted es apto para entrar al club, Felicidades !!!");
+        }
+    }
+}
